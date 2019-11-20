ad427d11e6243a4d057913097baf0f9af9f5a444
Bernice Cheung <bernice.fxx@gmail.com>
2019-11-19 16:31:48 -0800

    change mem
    allocate 150 GB memory

--- RSA_talapasTast.sh
+++ RSA_talapasTast.sh
@@ -5,11 +5,11 @@
 #SBATCH --error=trait_RSA.err          ### File in which to store job error messages
 #SBATCH --time=5-00:00:00       ### Wall clock time limit in Days-HH:MM:SS
 #SBATCH --cpus-per-task=4               ### Number of CPU needed for the job
-#SBATCH --mem=50G              ### Total memory
+#SBATCH --mem=150G              ### Total memory
 #SBATCH --account=sanlab      ### Account used for job submission
 
 module load prl
 module load python/2.7.13
 PYTHONPATH="${PYTHONPATH}:/projects/sanlab/kcheung3/traitNetwork_MVPA/RSA/src"
 export PYTHONPATH
-python /projects/sanlab/kcheung3/traitNetwork_MVPA/RSA/RSA.py --rootDir /projects/sanlab/kcheung3/traitNetwork_MVPA/ --ID 1 --valence P --rad 5
+python /projects/sanlab/kcheung3/traitNetwork_MVPA/RSA/RSA.py --rootDir /projects/sanlab/kcheung3/traitNetwork_MVPA/ --ID 1 --valence P --hemisphere W --rad 5
